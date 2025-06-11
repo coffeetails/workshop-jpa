c9f85071f418caa730c1107c9548fe71cafd4446
Monica Björk <kaffekod@protonmail.com>
2025-06-11 11:34:37 +0200

    set up db connection

---
+++ src/main/java/nu/kaffekod/workshopjpa/WorkshopJpaApplication.java
@@ -0,0 +1,15 @@
+package nu.kaffekod.workshopjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// http://localhost:8080
+
+@SpringBootApplication
+public class WorkshopJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nu/kaffekod/workshopjpa/WorkshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package nu.kaffekod.workshopjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
